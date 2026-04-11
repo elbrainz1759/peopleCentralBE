3549d5126b00eb5f9ee0a193ac2894fbd0185d3d
Ageh Jeremiah <agejerry442@gmail.com>
2026-04-11 23:47:11 +0100

    Fixing issues with the leave balance contorller

--- src/leave-types/leave-types.module.ts
+++ src/leave-types/leave-types.module.ts
@@ -3,6 +3,7 @@ import { LeaveTypesController } from './leave-types.controller';
 import { LeaveTypesService } from './leave-types.service';
 import { DatabaseModule } from '../database/database.module';
 
+//Module
 @Module({
   imports: [DatabaseModule],
   controllers: [LeaveTypesController],
