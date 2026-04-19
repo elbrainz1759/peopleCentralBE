69eac53e9b9ebfc39d5264aa4d2fd014f7d3c2c5
Ageh Jeremiah <agejerry442@gmail.com>
2026-04-19 13:29:11 +0100

    Fixing SQL import on leave type config

--- src/leave-type-configs/leave-type-configs.module.ts
+++ src/leave-type-configs/leave-type-configs.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LeaveTypeConfigsService } from './leave-type-configs.service';
 import { LeaveTypeConfigsController } from './leave-type-configs.controller';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
+
   controllers: [LeaveTypeConfigsController],
   providers: [LeaveTypeConfigsService],
   exports: [LeaveTypeConfigsService],
