9b59b77db72192917fc4331364d3179e407f250e
Ageh Jeremiah <agejerry442@gmail.com>
2026-05-12 12:56:42 +0100

    Fixing int error on leave type config

--- src/leave-type-configs/dto/create-leave-type-config.dto.ts
+++ src/leave-type-configs/dto/create-leave-type-config.dto.ts
@@ -1,5 +1,4 @@
 import {
-  IsInt,
   IsString,
   IsNotEmpty,
   IsNumber,
@@ -8,7 +7,7 @@ import {
 } from 'class-validator';
 
 export class CreateLeaveTypeConfigDto {
-  @IsInt()
+  @IsString()
   leaveTypeId!: string;
 
   @IsString()
