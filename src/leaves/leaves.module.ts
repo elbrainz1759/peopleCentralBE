7e2c878fe3e88f18cbf7f79db989e7d033fe27fe
Ageh Jeremiah <agejerry442@gmail.com>
2026-06-01 15:58:29 +0100

    Fixing error on leave module

--- src/leaves/leaves.module.ts
+++ src/leaves/leaves.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { LeavesController } from './leaves.controller';
 import { LeavesService } from './leaves.service';
 import { DatabaseModule } from '../database/database.module';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, MailModule],
   controllers: [LeavesController],
   providers: [LeavesService],
   exports: [LeavesService],
